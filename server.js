a8c8ac7eb3ea1c3163f1bf8eb0af2a4b5ea83450
gary merriman <g.merriman691@gmail.com>
2024-04-05 11:24:23 -0500

    save

--- server.js
+++ server.js
@@ -9,52 +9,50 @@ app.use(express.json());
 app.get(`/employees`, async (req, res) => {
    const employeeList = await db.many('SELECT * FROM employees')
    res.json(employeeList)
-
 });
 
 // Update the salary of all employees with a performance_rating of 5 by increasing it by 7%.
 
 app.put('/employees/updateSalary', async (req, res) => {
     await db.none('UPDATE employees SET salary = salary * 1.07 WHERE performance_rating = $1', [5])
-    res.send('salary updated')
+    res.send('Salary updated')
 });
 
 // Delete the employee with the lowest performance_rating.
 
-app.delete(`employees/deleteLowest`, async (req, res) => {
-    await db.none('DELETE FROM employees WHERE id = SELECT id FROM employees ORDER BY performance_rating ACS LIMIT 1')
-    res.send('lowest employee deleted')
+app.delete(`/employees/deleteLowest`, async (req, res) => {
+    await db.none('DELETE FROM employees WHERE id = (SELECT id FROM employees ORDER BY performance_rating ASC LIMIT 1)')
+    res.send('Lowest-rated employee deleted')
 });
 
-//Show all employees in the Sales department.
+// Show all employees in the Sales department.
 
 app.get(`/employees/sales`, async (req, res) => {
-   const salesEmployees = await db.many('SELECT * FROM employees WHERE department ="sales"')
+   const salesEmployees = await db.many('SELECT * FROM employees WHERE department = \'Sales\'')
     res.json(salesEmployees)
 });
 
-//Show the highest performance_rating in the IT department.
+// Show the highest performance_rating in the IT department.
 
 app.get(`/employees/highestInIt`, async (req, res) => {
-   const highestInIt = await db.one('SELECT MAX(performance_rating) WHERE employees WHERE department = "IT"')
-    res.send(highestInIt)
+   const highestInIt = await db.one('SELECT MAX(performance_rating) FROM employees WHERE department = \'IT\'')
+    res.send(`Highest performance rating in the IT department: ${highestInIt}`)
 });
 
-//Update the table so that all employees hired after 2015 have their performance_rating increased by 1.
+// Update the table so that all employees hired after 2015 have their performance_rating increased by 1.
 
 app.put(`/employees/updatePerformanceAfter2015`, async (req, res) => {
-    await db.none('UPDATE employees SET performance_rating + 1 WHERE hire_date > "2015-01-01"')
-    res.send('updated performance')
-})
-
-//Show the names and salaries of employees with a performance_rating greater than 4.
-
-app.get(`employees/highPerformers`, async (req, res) => {
-   const highPerformers = await db.any('SELECT name, salary FROM employees WHERE performance_rating > 4')
-   res.send(highPerformers)
+    await db.none('UPDATE employees SET performance_rating = performance_rating + 1 WHERE hire_date > \'2015-01-01\'')
+    res.send('Updated performance ratings after 2015')
 });
 
+// Show the names and salaries of employees with a performance_rating greater than 4.
+
+app.get(`/employees/highPerformers`, async (req, res) => {
+   const highPerformers = await db.any('SELECT name, salary FROM employees WHERE performance_rating > 4')
+   res.json(highPerformers)
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}.`);
-     });
+});
